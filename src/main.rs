223d903aff4acc98f57e31192f03812fbdc3557f
Elijah Insua <tmpvar@gmail.com>
2019-09-21 18:30:43 -0700

    use BufReader for read_line and fix some bugs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::io;
+use std::io::*;
 use std::time::Duration;
-// use std::io::prelude::*;
-use std::thread;
 
 extern crate clap;
 extern crate serialport;
@@ -12,7 +10,7 @@ use clap::{App, AppSettings, Arg};
 use colored::*;
 use serialport::prelude::*;
 
-fn main() {
+fn main() -> Result<()> {
     let matches = App::new("Serialport Example - Receive Data")
         .about("Reads data from a serial port and echoes it to stdout")
         .setting(AppSettings::DisableVersion)
@@ -41,124 +39,61 @@ fn main() {
         ::std::process::exit(1);
     }
 
-    match serialport::open_with_settings(&port_name, &settings) {
-        Ok(mut port) => {
-            // Clone the port
-            let mut clone = port.try_clone().expect("Failed to clone");
+    let mut port = serialport::open_with_settings(&port_name, &settings)?;
+    // let mut serial_buf: Vec<u8> = vec![0; 1000];
+    println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+    let mut reader = BufReader::new(&mut port);
+    loop {
+        // let mut slice = serial_buf.as_mut_slice();
+        // let mut start = 0;
+        let mut serial_str = String::new();
+        // match port.read(&mut slice[start..]) {
+        match reader.read_line(&mut serial_str) {
+            Ok(t) => {
+                if t == 0 {
+                    continue;
+                }
 
-            // Send out 4 bytes every second
-            thread::spawn(move || loop {
-                clone
-                    .write(&['?' as u8])
-                    .expect("Failed to write to serial port");
-                thread::sleep(Duration::from_millis(1000));
-            });
+                if serial_str.find("ok") == None && serial_str.find("Grbl") != None {
+                    continue;
+                }
 
-            let mut serial_buf: Vec<u8> = vec![0; 1000];
-            println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
-            loop {
-                match port.read(serial_buf.as_mut_slice()) {
-                    Ok(t) => {
-                        io::stdout().write_all(&serial_buf[..t]).unwrap();
+                println!("{}", serial_str.trim());
 
-                        // skip status lines
-                        if serial_buf[0] == '<' as u8 {
-                            continue;
-                        }
+                loop {
+                    let mut input = String::new();
+                    match stdin().read_line(&mut input) {
+                        Ok(_) => {
+                            let trimmed = input.as_str().trim();
 
-                        loop {
-                            let mut input = String::new();
-                            match io::stdin().read_line(&mut input) {
-                                // TODO: if we read an empty line from stdin, discard this one and read
-                                //       another.
+                            // skip this
+                            if trimmed.len() == 0 {
+                                continue;
+                            }
+                            let line = format!("{}\n", trimmed);
+                            print!("{} :: ", trimmed.white());
+
+                            let bytes = line.as_bytes();
+                            match reader.get_mut().write_all(&bytes[..]) {
                                 Ok(_) => {
-                                    let trimmed = input.as_str().trim();
-
-                                    // skip this
-                                    if trimmed.len() == 0 {
-                                        continue;
-                                    }
-                                    let line = format!("{}\n", trimmed);
-                                    println!("{}", trimmed.white());
-                                    //input.push_str("\n");
-                                    let bytes = line.as_bytes();
-                                    match port.write_all(&bytes[..]) {
-                                        Ok(_) => {
-                                            break;
-                                        }
-                                        Err(e) => {
-                                            eprintln!("could not write to sp. Error: {}", e);
-                                            break;
-                                        }
-                                    }
+                                    break;
                                 }
-
                                 Err(e) => {
-                                    eprintln!("Failed to read from stdin, Error: {}", e);
+                                    eprintln!("could not write to sp. Error: {}", e);
                                     break;
                                 }
                             }
                         }
+
+                        Err(e) => {
+                            eprintln!("Failed to read from stdin, Error: {}", e);
+                            break;
+                        }
                     }
-                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                    Err(e) => eprintln!("{:?}", e),
                 }
             }
-        }
-        Err(e) => {
-            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
-            ::std::process::exit(1);
+            Err(ref e) if e.kind() == ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
         }
     }
 }
-
-// fn main() {
-//     for arg in env::args_os().skip(1) {
-//         println!("open port {:?}", arg);
-//         let mut port = serial::open(&arg).unwrap();
-//         interact(&mut port).unwrap();
-//     }
-// }
-//
-// fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
-//     port.reconfigure(&|settings| {
-//
-//         settings.set_baud_rate(serial::Baud115200)?;
-//         settings.set_char_size(serial::Bits8);
-//         settings.set_parity(serial::ParityNone);
-//         settings.set_stop_bits(serial::Stop1);
-//         settings.set_flow_control(serial::FlowNone);
-//         Ok(())
-//     })?;
-//
-//     port.set_timeout(Duration::from_millis(1000))?;
-//     let mut header = String::new();
-//
-//     port.read_to_string(&mut header)?;
-//
-//     println!("read header {}", header);
-//
-//     loop {
-//         let mut input = String::new();
-//         let len = io::stdin().read_line(&mut input)?;
-//         if len == 0 {
-//             break;
-//         }
-//         println!("stdin: {}", input);
-//
-//         //let bytes = input.clone().into_bytes();
-//
-//         //port.write(&bytes)?;
-//         port.write(b"GX100F100\n")?;
-//
-//         println!("send: {}", input);
-//         // let mut buf: Vec<u8> = (0..255).collect();
-//         let mut recv_line = String::new();
-//         port.read_to_string(&mut recv_line)?;
-//         // let recv_bytes = port.read(&mut buf[..])?;
-//         // println!("recv: {:?}", String::from_utf8(buf));
-//         println!("recv: {}", recv_line);
-//     }
-//
-//     Ok(())
-// }
